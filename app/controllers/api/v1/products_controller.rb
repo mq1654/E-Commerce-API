d29900bc4dddcffc0a7aa75f0d4264c3a1b6415c
Steve Huynh <quanhmse181780@fpt.edu.vn>
2026-09-16 17:38:16 +0700

    feat: add product index and show actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -52,6 +52,16 @@ class Api::V1::ProductsController < ApplicationController
       end
    end
 
+   def index
+      products = Product.all
+      render json: products, status: :ok
+   end
+
+   def show
+      product = Product.find(params[:id])
+      render json: product, status: :ok
+   end
+
    private
    def product_params
       params.require(:product).permit(
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :products, only: [ :create ]
+      resources :products, only: [ :create, :index, :show ]
     end
   end
 end
